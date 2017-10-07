b2119d1cd3a62ff1ac2dd7b22c6baf9d34c7b3c4
Vandenborne Stefan <stefan@illutek.be>
2017-10-07 13:12:16 +0200

    odd.inc.php verder layouten

--- src/partical/odd.inc.php
+++ src/partical/odd.inc.php
@@ -12,20 +12,34 @@
 
   <div class="summit-discription-wrap">
     <div class="summit-description">
+
+      <!-- Description of project here -->
       <h1>Title</h1>
       <h2>Subtitle</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, rerum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cupiditate dicta libero, molestiae obcaecati
-        perspiciatis!</p>
+        perspiciatis! um dolor sit amet, consectetur adipisicing elit. Alias </p>
+
     </div>
+
     <div class="summit-icons-wrap">
+
+      <!-- Icons of used technology here -->
       <img src="images/sass.png" alt="sass">
       <img src="images/gulp.png" alt="gulp">
+      <img src="images/drupal8.png" alt="Drupal8">
+
     </div>
+
   </div>
 
+  <div class="divider"></div>
+
   <div class="summit-images-wrap">
-    Images
+
+    <!-- Image of project here -->
+    <img src="images/cityMobile-640W.jpg" alt="test">
+
   </div>
 
   <div class="arrow arrow-down">
